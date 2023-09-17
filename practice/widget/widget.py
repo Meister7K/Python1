2f228c68a5be0e239024ddcd32a5b0db973b6a94
Karl Finkel <kmanfinkel17@gmail.com>
2023-09-16 21:34:44 -0600

    added counter

--- practice/widget/widget.py
+++ practice/widget/widget.py
@@ -1,10 +1,25 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 from kivy.metrics import dp 
+from kivy.properties import StringProperty
+
+class ButtonClickEx(GridLayout):
+    
+    my_text=StringProperty("0")
+    count = 1
+    def on_button_click(self):
+        print("clicked")
+        self.my_text = str(self.count)
+        self.count += 1
+    def on_reset_click(self):
+        print("reset")
+        self.my_text = str(0)
+        self.count = 1
 
 
 class WidgetApp(App):
-    pass
+    pass 
 
 WidgetApp().run()
