60ddc5ecf6d6ac077e4209018e3f2a0aa75199fb
Karl Finkel <kmanfinkel17@gmail.com>
2023-09-23 14:42:12 -0600

    added movement buttons and a ball

--- practice/canvas/Canvas.py
+++ practice/canvas/Canvas.py
@@ -3,7 +3,7 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.metrics import dp 
-from kivy.properties import StringProperty, BooleanProperty
+from kivy.properties import StringProperty, BooleanProperty, Clock
 from kivy.graphics.vertex_instructions import Line, Rectangle, Ellipse
 from kivy.graphics.context_instructions import Color
 
@@ -29,12 +29,31 @@ class CanvasEx2(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.ball_size = dp(50)
+        self.velX =dp(5)
+        self.velY =dp(5)
         with self.canvas:
             Color(1,1,1)
             self.rect = Rectangle(pos=(0,400), size=(100,100))
             self.ball = Ellipse(pos=self.center, size=(self.ball_size,self.ball_size))
+        Clock.schedule_interval(self.update, 1/60)
     def on_size(self, *args):
-        self.ball.pos = ()
+        self.ball.pos = (self.center_x -self.ball_size/2, self.center_y - self.ball_size/2)
+    def update(self, dt):
+
+        x,y = self.ball.pos
+
+        x+=self.velX
+        y+=self.velY
+
+        if y + self.ball_size >self.height:
+            self.velY = -self.velY
+        if x + self.ball_size >self.width:
+            self.velX = -self.velX
+        if y <0:
+            self.velY = -self.velY
+        if x < 0:
+            self.velX = -self.velX
+        self.ball.pos = (x, y)
 
     def right_button_click(self):
         move_dist = dp(100)
